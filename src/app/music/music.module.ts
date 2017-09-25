c0159f914421095b69eec9ab296db1708975890c
Purna Chander Mashetty <purnachander.mashetty@capitalone.com>
2017-09-25 13:42:58 -0500

    added primeng

--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
 import { CommonModule } from '@angular/common';
+import { AutoCompleteModule } from 'primeng/primeng';
 import { MusicDetailsComponent } from './music-details/music-details.component';
 import { MusicFooterComponent } from './music-footer/music-footer.component';
 import { MusicProgressComponent } from './music-progress/music-progress.component';
 import { MusicSearchComponent } from './music-search/music-search.component';
+import { MusicPlayerComponent } from './music-player/music-player.component';
+import { MusicService } from './shared/music.service';
+import { ApiService } from './shared/api.service';
 
 @NgModule({
   imports: [
+    // Define imports
+    FormsModule,
+    AutoCompleteModule,
+    HttpModule,
     CommonModule
   ],
-  declarations: [MusicDetailsComponent, MusicFooterComponent, MusicProgressComponent, MusicSearchComponent]
+  exports: [
+    // Expose components
+    MusicSearchComponent,
+    MusicDetailsComponent,
+    MusicPlayerComponent,
+    MusicProgressComponent,
+    MusicFooterComponent
+  ],
+  providers: [
+    // Services
+    ApiService,
+    MusicService
+  ],
+  declarations: [MusicDetailsComponent, MusicFooterComponent, MusicProgressComponent, MusicSearchComponent, MusicPlayerComponent]
 })
 export class MusicModule { }
