86746664b4f3613d14e9b1734502668ab6771570
Purna Chander Mashetty <purnachander.mashetty@capitalone.com>
2017-09-26 11:06:53 -0500

    updated services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MusicService } from './music/shared/music.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title;
+  filteredTracks: any[] = [];
+  backgroundStyle;
+
+  constructor(
+    private musicService: MusicService
+  ) { }
+
+  handleQuery(payload) {
+    this.musicService.findTracks(payload).subscribe(tracks => {
+      this.filteredTracks = tracks;
+    });
+  }
+
+  handleUpdate(track) {
+    this.musicService.play(track.stream_url);
+    this.title = track.title;
+    this.backgroundStyle = this.composeBackgroundStyle(track.artwork_url)
+  }
+
+  composeBackgroundStyle(url) {
+    return {
+      width: '100%',
+      height: '1000px',
+      backgroundSize: 'cover',
+      backgroundImage: `linear-gradient(
+    rgba(0, 0, 0, 0.7),
+    rgba(0, 0, 0, 0.7)
+  ),   url(${this.musicService.xlArtwork(url)})`
+    }
+  }
+
 }
--- src/app/music/shared/music.service.ts
+++ src/app/music/shared/music.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Injectable()
 export class MusicService {
+  audio;
+  constructor(
+    private apiService: ApiService
+  ) {
+    this.audio = new Audio();
+  }
+  findTracks(value) {
+    return this.apiService.get(`${this.apiService.prepareUrl('https://api.soundcloud.com/tracks')}&q=${value}`, false)
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .map(res => res.json())
+  }
 
-  constructor() { }
+  load(url) {
+    this.audio.src = this.apiService.prepareUrl(url);
+    this.audio.load();
+  }
+
+  play(url) {
+    this.load(url);
+    this.audio.play()
+  }
+
+  xlArtwork(url) {
+    return url.replace(/large/, 't500x500');
+  }
 
 }
