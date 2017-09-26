b9032dace7992f227b3056986ce5a9b0e7cebcc9
Purna Chander Mashetty <purnachander.mashetty@capitalone.com>
2017-09-26 09:52:41 -0500

    updated music-search component

--- src/app/music/music-search/music-search.component.ts
+++ src/app/music/music-search/music-search.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-music-search',
+  selector: 'music-search',
   templateUrl: './music-search.component.html',
   styleUrls: ['./music-search.component.css']
 })
 export class MusicSearchComponent implements OnInit {
 
   constructor() { }
+
+  track: string;
+  @Input() tracks: any[];
+
+  @Output() update = new EventEmitter();
+  @Output() query = new EventEmitter();
+
+  search(event) {
+    this.query.emit(event.query);
+  }
+
+  select(track) {
+    this.update.emit(track);
+  }
 
   ngOnInit() {
   }
