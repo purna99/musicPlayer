08cac754b8c5ccf4760272e1bc33a76cf99971e8
purna99 <cpurna1@gmail.com>
2017-09-29 19:29:19 -0500

    updated player component

--- src/app/music/music-player/music-player.component.ts
+++ src/app/music/music-player/music-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input. Output } from '@angular/core';
 
 @Component({
   selector: 'app-music-player',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./music-player.component.css']
 })
 export class MusicPlayerComponent implements OnInit {
+  // If song is paused or playing
+  @Input() paused;
+  // Controls
+  @Output() backward = new EventEmitter();
+  @Output() pauseplay = new EventEmitter();
+  @Output() forward = new EventEmitter();
+  @Output() random = new EventEmitter();
+  @Output() stop = new EventEmitter();
 
   constructor() { }
 
