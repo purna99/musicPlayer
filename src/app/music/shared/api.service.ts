2f326de156be9ed4eaa9ceb9dd75808373f88f65
purna99 <cpurna1@gmail.com>
2017-09-30 15:15:29 -0500

    updated api-service

--- src/app/music/shared/api.service.ts
+++ src/app/music/shared/api.service.ts
@@ -4,21 +4,25 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ApiService {
 
-  clientId = '2f98992c40b8edf17423d93bda2e04ab'
+    clientId = '[CLIENT_ID]'
 
-  constructor(
-    private http: Http
-  ) { }
+    constructor(
+      private http: Http
+    ) {}
 
-  get(url, attachClientId?) {
-  let u;
-  attachClientId ? u = this.prepareUrl(url) : u = url;
-  return this.http.get(u);
-}
+    get(url, attachClientId?) {
+      // Should attach client id if the attachToken
+      // is true
+      let u;
+      attachClientId ? u = this.prepareUrl(url) : u = url;
+      // Returns an obsrevable
+      // for the HTTP get request
+      return this.http.get(u);
+    }
 
-prepareUrl(url) {
-  //Attach client id to stream url
-  return `${url}?client_id=${this.clientId}`
-}
+    prepareUrl(url) {
+      //Attach client id to stream url
+      return `${url}?client_id=${this.clientId}`
+    }
 
 }
