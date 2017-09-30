d55f8ab4926a769c16d6e787778dc4c7716c94cc
purna99 <cpurna1@gmail.com>
2017-09-29 19:06:49 -0500

    updated details component

--- src/app/music/music-details/music-details.component.ts
+++ src/app/music/music-details/music-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-music-details',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./music-details.component.css']
 })
 export class MusicDetailsComponent implements OnInit {
+
+  @Input() title: string;
 
   constructor() { }
 
